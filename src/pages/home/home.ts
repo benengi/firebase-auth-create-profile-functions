a4c900e32c834545ab5779c9a85a3e24983f83f2
Jorge Vergara <javebratt@gmail.com>
2017-05-09 14:35:13 -0500

    Created function to add userProfile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
 
 @IonicPage()
 @Component({
@@ -9,18 +7,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public userProfile:FirebaseObjectObservable<any>;
-  public userId:string;
 
-  constructor(public navCtrl: NavController, public afDatabase: AngularFireDatabase, 
-  public afAuth: AngularFireAuth) {
-    this.afAuth.authState.subscribe( user => {
-      if (user) {
-        this.userProfile = this.afDatabase.object(`userProfile/${user.uid}/`);
-      } else {
-        this.userProfile = null;
-      }
-    });
-  }
+  constructor(public navCtrl: NavController) {}
 
 }
